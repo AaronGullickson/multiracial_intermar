78cc69874af428c13abe7eb10eb9be52c296279e
Aaron Gullickson <aarongullickson@gmail.com>
2023-08-03 14:45:24 -0700

    Add full models to analysis; other touchups

--- utils/functions.R
+++ utils/functions.R
@@ -1,2 +1,14 @@
 # functions.R
 
+#A function to convert model summaries to something knitreg will understand
+convertModel <- function(model) {
+  tr <- createTexreg(
+    coef.names = rownames(model$coef), 
+    coef = model$coef[,"b.pool"], 
+    se = model$coef[,"se.pool"], 
+    pvalues = model$coef[,"pvalue.pool"],
+    gof.names = c("Deviance","BIC (relative to null)"), 
+    gof = c(mean(model$deviance), mean(model$bic)), 
+    gof.decimal = c(FALSE, FALSE)
+  )
+}
